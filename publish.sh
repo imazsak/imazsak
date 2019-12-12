f982ee812f8ba08ac799e4f4b0e11b1b51d3b7ac
Ksisu <horkris@gmail.com>
2019-12-12 17:21:58 +0100

    Remove old ops scripts

--- publish.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker build -t ksisu/imazsak-core .
-docker tag ksisu/imazsak-core rg.fr-par.scw.cloud/imazsak/imazsak-core
-docker push rg.fr-par.scw.cloud/imazsak/imazsak-core
